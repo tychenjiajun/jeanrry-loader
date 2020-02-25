9b3139f25dfe1c719113703d3854cfef57502558
Jiajun Chen <tychenjiajun@live.cn>
2020-02-25 18:41:50 +0800

    feat: add loader options

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const qs = require('querystring')
-const frenchkiss = require('frenchkiss')
 const compiler = require('vue-template-compiler')
+const utils = require('loader-utils')
+
+const defaultOption = {
+    translator: 'frenchkiss',
+    queryName: 'jeanrry',
+    blockName: 'jeanrry',
+    locale: 'en',
+    fallback: 'en',
+    functionName: 't',
+    remove: true
+}
+
 
 
 module.exports = function (source) {
     const query = qs.parse(this.resourceQuery.slice(1))
 
-    if (query.type === 'template' && query.jeanrry != null) {
-        frenchkiss.locale(query.jeanrry)
+    const mergedOption = { ...defaultOption, ...utils.getOptions(this) }
+    const queryName = mergedOption.queryName
+    const blockName = mergedOption.blockName
+
+    const frenchkiss = require(mergedOption.translator)
+
+    if (query.type === 'template' && query[queryName] != null && query[queryName] !== 'false') {
+        query[queryName] === 'true' ? frenchkiss.locale(mergedOption.locale) : frenchkiss.locale(query[queryName])
+        frenchkiss.fallback(mergedOption.fallback)
+
+        const functionName = mergedOption.functionName
+        const functionNameLength = functionName.length
         const sfc = compiler.parseComponent(source)
-        const langSettings = JSON.parse(sfc.customBlocks.find(({type}) => type === 'jeanrry').content)
+        const langSettings = JSON.parse(sfc.customBlocks.find(({ type }) => type === blockName).content)
         for (key of Object.keys(langSettings)) {
             frenchkiss.set(key, langSettings[key])
         }
@@ -18,8 +39,8 @@ module.exports = function (source) {
         let end = sfc.template.end
         let result = source.substring(0, current)
         while (current < end) {
-            const stack = ['(']
-            const functionStart = source.indexOf('t(', current + 1) + 2 // skip the t and '('
+            const stack = ['('] // the start of the function
+            const functionStart = source.indexOf(functionName + '(', current + 1) + functionNameLength + 1 // skip the function name and '(', the start of the first param
             if (functionStart === 1) break
             let pointer = functionStart
             let stringType = ''
@@ -94,7 +115,7 @@ module.exports = function (source) {
             }
             if (stack.length === 0 && pointer < end + 1) {
                 const params = new Function('"use strict";return [' + source.substring(functionStart, pointer - 1) + ']')() // remove the trailing bracket
-                result = result + source.substring(current, functionStart - 2) + frenchkiss.t(...params)
+                result = result + source.substring(current, functionStart - functionNameLength - 1) + frenchkiss.t(...params)
                 current = pointer
             } else {
                 result = result + source.substring(current, functionStart)
@@ -103,11 +124,11 @@ module.exports = function (source) {
         }
         result = result + source.substring(current)
         this.callback(null, result)
-    } 
-    else if (query.type === 'custom' && query.blockType === 'jeanrry') { // skip to avoid 'You may need an additional loader to handle the result of these loaders.'
+    }
+    else if (mergedOption.remove && query.type === 'custom' && query.blockType === blockName) { // skip to avoid 'You may need an additional loader to handle the result of these loaders.'
         const sfc = compiler.parseComponent(source)
-        const block = sfc.customBlocks.find(({type}) => type === 'jeanrry')
-        this.callback(null, source.substring(0, block.start) + source.substring(block.end)) 
+        const block = sfc.customBlocks.find(({ type }) => type === blockName)
+        this.callback(null, source.substring(0, block.start) + source.substring(block.end))
     }
     else {
         this.callback(null, source)
